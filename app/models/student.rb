a96096674ac3d4008a1112e27eb21b6b5a7c56ed
Daniel Lewis <Lewis411@live.marshall.edu>
2015-10-21 20:48:38 -0400

    Finished student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
-  validates :name,  presence: true, length: { maximum: 8 }
+  validates :name,  presence: true, length: { maximum: 8 },
+  					uniqueness: { case_sensitive: false }
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -11,12 +11,18 @@ class StudentTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-  	@student.name = "     "
+  	@student.name = ""
   	assert_not @student.valid?
   end
 
   test "name should not be too long" do
-  	@student.name = "a" * 8
-  	assert_not @student.valid?
+    @student.name = "a" * 9
+    assert_not @student.valid?
+  end
+
+  test "student names should be unique" do
+  	duplicate_student = @student.dup
+  	@student.save
+  	assert_not duplicate_student.valid?
   end
 end
