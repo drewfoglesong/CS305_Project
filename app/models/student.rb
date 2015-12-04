e3f90efb4b832633d1e40aa3a1cc97313dcaa658
Daniel Lewis <Lewis411@live.marshall.edu>
2015-12-04 00:28:36 -0500

    Few more error fixes

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,8 +3,4 @@ has_many :scores
 validates :name,  presence: true, length: { maximum: 6 },
   					uniqueness: { case_sensitive: false },
   					format: { with: /\b[a-zA-Z]{4}\d{2}\b/ }
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/Presentation
 end
